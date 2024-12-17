251c9b659d69e13c5dad22e69df5f75df8c32557
Umeno Taisei <takoyakitaimuda417@gmail.com>
2024-12-17 09:23:46 +0900

    12/17日の内容を追加

--- app8.js
+++ app8.js
@@ -1,5 +1,8 @@
+"use strict";
 const express = require("express");
 const app = express();
+
+let bbs = [];  // 本来はDBMSを使用するが，今回はこの変数にデータを蓄える
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
@@ -79,29 +82,52 @@ app.post("/add", (req, res) => {
   res.json( {answer: num1+num2} );
 });
 
-app.post("/janken", (req, res) => {
-  let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
-  let cpu = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
-  else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
-  total += 1;
-  const display = {
-    your: hand,
-    cpu: cpu,
-    judgement: judgement,
-    win: win,
-    total: total
-  }
-  res.render( 'janken', display );
+// これより下はBBS関係
+app.post("/check", (req, res) => {
+  // 本来はここでDBMSに問い合わせる
+  res.json( {number: bbs.length });
+});
+
+app.post("/read", (req, res) => {
+  // 本来はここでDBMSに問い合わせる
+  const start = Number( req.body.start );
+  console.log( "read -> " + start );
+  if( start==0 ) res.json( {messages: bbs });
+  else res.json( {messages: bbs.slice( start )});
+});
+
+app.post("/post", (req, res) => {
+  const name = req.body.name;
+  const message = req.body.message;
+  console.log( [name, message] );
+  // 本来はここでDBMSに保存する
+  bbs.push( { name: name, message: message } );
+  res.json( {number: bbs.length } );
+});
+
+app.get("/bbs", (req,res) => {
+    console.log("GET /BBS");
+    res.json( {test: "GET /BBS" });
+});
+
+app.post("/bbs", (req,res) => {
+    console.log("POST /BBS");
+    res.json( {test: "POST /BBS"});
+})
+
+app.get("/bbs/:id", (req,res) => {
+    console.log( "GET /BBS/" + req.params.id );
+    res.json( {test: "GET /BBS/" + req.params.id });
+});
+
+app.put("/bbs/:id", (req,res) => {
+    console.log( "PUT /BBS/" + req.params.id );
+    res.json( {test: "PUT /BBS/" + req.params.id });
+});
+
+app.delete("/bbs/:id", (req,res) => {
+    console.log( "DELETE /BBS/" + req.params.id );
+    res.json( {test: "DELETE /BBS/" + req.params.id });
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
